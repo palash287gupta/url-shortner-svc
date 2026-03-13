4021660552beae73feef236438ddf4ae4d63e664
Palash Gupta <palash.gupta287@gmail.com>
2026-03-13 19:29:38 +0530

    add storage maps and short code generator

--- main.go
+++ main.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"log"
+	"math/rand"
 	"net/http"
+	"sync"
+	"time"
 )
 
+var (
+	urlMap     = make(map[string]string) // short code -> original URL
+	reverseMap = make(map[string]string) // original URL -> short code
+	mu         sync.RWMutex
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func generateShortCode() string {
+	b := make([]byte, 6)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	port := ":8080"
 	log.Printf("Starting server on port %s", port)
 
